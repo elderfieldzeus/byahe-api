1dd28d3ca724fdd5a31ff9f0aee34daaacc2be6b
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-03-19 00:08:27 +0800

    refactor: changed .env variable names

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,10 @@
-export const SEQUELIZE_HOST = process.env.MYSQL_HOST ?? 'localhost';
-export const SEQUELIZE_PORT = process.env.MYSQL_PORT
-  ? Number(process.env.MYSQL_PORT)
+export const SEQUELIZE_HOST = process.env.SQL_HOST ?? 'localhost';
+export const SEQUELIZE_PORT = process.env.SQL_PORT
+  ? Number(process.env.SQL_PORT)
   : 3306;
-export const SEQUELIZE_USERNAME = process.env.MYSQL_USERNAME ?? 'root';
-export const SEQUELIZE_PASSWORD = process.env.MYSQL_PASSWORD ?? '';
-export const SEQUELIZE_DATABASE = process.env.MYSQL_DATABASE ?? 'byahe_db';
+export const SEQUELIZE_USERNAME = process.env.SQL_USERNAME ?? 'root';
+export const SEQUELIZE_PASSWORD = process.env.SQL_PASSWORD ?? '';
+export const SEQUELIZE_DATABASE = process.env.SQL_DATABASE ?? 'byahe_db';
 
 export const JWT_SECRET = process.env.JWT_SECRET ?? 'secretKey';
 
