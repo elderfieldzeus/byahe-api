b138a915e1dc627838bda61488f993bc9069a9ae
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-03-21 12:46:23 +0800

    refactor: changed port

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const SEQUELIZE_HOST = process.env.SQL_HOST ?? 'localhost';
 export const SEQUELIZE_PORT = process.env.SQL_PORT
   ? Number(process.env.SQL_PORT)
-  : 5432;
+  : 3306;
 export const SEQUELIZE_USERNAME = process.env.SQL_USERNAME ?? 'root';
 export const SEQUELIZE_PASSWORD = process.env.SQL_PASSWORD ?? '';
 export const SEQUELIZE_DATABASE = process.env.SQL_DATABASE ?? 'byahe_db';
